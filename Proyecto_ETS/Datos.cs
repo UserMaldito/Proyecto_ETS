caf5035433effb1d7b9935c42f3c6865d2f240db
Daniel Alvarez <dan.alv.ard97@gmail.com>
2022-03-21 13:18:47 +0000

    Tras error en metodología de trabajo, agrego la parte del proyecto de introduccion de fechas, falta documentacion

--- Proyecto_ETS/Datos.cs
+++ Proyecto_ETS/Datos.cs
@@ -6,8 +6,229 @@ using System.Threading.Tasks;
 
 namespace Proyecto_ETS
 {
-	class Datos
-	{
-		//comentario
-	}
-}
+    class Datos
+    {
+        public struct Fechas
+        {
+            public DateTime Fecha;
+            public bool AC;
+        }
+        /// <summary>
+        /// Función que recoge las funciones necesarias para generar un array int con una fecha válida (solo DC)
+        /// </summary>
+        /// <returns>Array Int con dia mes y año</returns>
+        public static int[] ObtenerFecha()
+        {
+            bool bisiesto = false;
+            int ano = Datos.ObtenerAno(ref bisiesto);
+            int mes = Datos.ObtenerMes();
+            int dia = Datos.ObtenerDia(mes, bisiesto);
+            int[] fecha = Datos.MontarFecha(ano, mes, dia);
+            return (fecha);
+        }
+        /// <summary>
+        /// Pide al usuario que introduzca un número entero para almacenarlo como año
+        /// </summary>
+        /// <param name="bisiesto"> booleano que informara si el año es bisiesto o no</param>
+        /// <returns>Int con la información atribuida a año, asi como si el año es bisiesto o no</returns>
+        public static int ObtenerAno(ref bool bisiesto)
+        {
+            int ano;
+            bool siguiente = false;
+            do
+            {
+                Console.WriteLine("Introduzca el año a tratar (en número)");
+                if (Int32.TryParse(Console.ReadLine(), out ano))
+                {
+                    if (ano >= 0)
+                    {
+                        if (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0)
+                        {
+                            bisiesto = true;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("el año no puede ser negativo o 0");
+                    }
+
+
+                    siguiente = true;
+                }
+                else
+                {
+                    Console.WriteLine("El año introducido no es un número válido");
+                }
+            } while (!siguiente);
+            return ano;
+        }
+        /// <summary>
+        /// Pide al usuario que introduzca un número entero para almacenarlo como mes
+        /// </summary>
+        /// <returns>Int que almacenara el mes introducido por el usuario, por referencia si el año es bisiesto o no</returns>
+        public static int ObtenerMes()
+        {
+            int mes;
+            bool siguiente = false;
+            do
+            {
+                Console.WriteLine("Introduzca el mes a tratar (en número)");
+                if (Int32.TryParse(Console.ReadLine(), out mes))
+                {
+                    if (mes > 0)
+                    {
+                        if (mes < 13)
+                        {
+                            siguiente = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("El mes no puede ser mayor de 12.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("El mes no puede ser menor de 0.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No ha introducido un dato numérico.");
+                }
+            } while (!siguiente);
+            return mes;
+        }
+        /// <summary>
+        /// Funcion que obtiene el dia, comprobando si es válido
+        /// </summary>
+        /// <param name="mes">Int que almacena el mes introducido por el usuario</param>
+        /// <param name="bisiesto"> booleano que informara si el año es bisiesto o no</param>
+        /// <returns>Int que almacena el dia introducido por el usuario</returns>
+        public static int ObtenerDia(int mes, bool bisiesto)
+        {
+            int dia;
+            bool siguiente = false;
+            string errordia = "El día introducido se sale de los rangos validos";
+            do
+            {
+                Console.WriteLine("Introduzca el día a tratar");
+                if (Int32.TryParse(Console.ReadLine(), out dia))
+                {
+                    if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
+                    {
+                        if (dia >= 1 && dia <= 31)
+                        {
+                            siguiente = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine(errordia);
+                        }
+                    }
+                    else
+                    {
+                        if (mes != 2)
+                        {
+                            if (dia >= 1 && dia <= 30)
+                            {
+                                siguiente = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine(errordia);
+                            }
+                        }
+                        else
+                        {
+                            if (bisiesto)
+                            {
+                                if (dia >= 1 && dia <= 29)
+                                {
+                                    siguiente = true;
+                                }
+                                else
+                                {
+                                    Console.WriteLine(errordia);
+                                }
+                            }
+                            else
+                            {
+                                if (dia >= 1 && dia <= 28)
+                                {
+                                    siguiente = true;
+                                }
+                                else
+                                {
+                                    Console.WriteLine(errordia);
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No ha introducido un dato numérico");
+                }
+            } while (!siguiente);
+            return dia;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ano">Int donde almacenamos el año</param>
+        /// <param name="mes">Int donde se almacena el mes</param>
+        /// <param name="dia">Int donde se almacena el día</param>
+        /// <returns></returns>
+        public static int[] MontarFecha(int ano, int mes, int dia)
+        {
+            int[] fecha = new int[3];
+            fecha[0] = dia;
+            fecha[1] = mes;
+            fecha[2] = ano;
+            return fecha;
+
+        }
+        public static DateTime crearDateTime(int[] fecha)
+        {
+            DateTime modFecha = new DateTime(fecha[2], fecha[1], fecha[0]);
+            return modFecha;
+        }
+        public static bool ObtenerAC()
+        {
+            bool siguiente = false;
+            bool AC = false;
+            int numIntrod;
+            do
+            {
+                Console.WriteLine("Pulse 1 para año antes de Cristo, pulse 2 para despues de cristo");
+                if (int.TryParse(Console.ReadLine(), out numIntrod))
+                {
+                    if (numIntrod == 1)
+                    {
+                        AC = true;
+                        siguiente = true;
+                    }
+
+                    else
+                    {
+                        if (numIntrod == 2)
+                        {
+                            AC = false;
+                            siguiente = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ha introducido un número diferente");
+                        }
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ha introducido un dato en formato no válido");
+                }
+            } while (!siguiente);
+            return AC;
+        }
+
+    }
